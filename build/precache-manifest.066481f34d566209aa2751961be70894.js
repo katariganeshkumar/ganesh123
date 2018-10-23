cc2ef7a9afa9cbcef0c0cb2acc87244b112e266a
Ganesh <katariganeshkumar@gmail.com>
2018-10-23 22:53:01 +0530

    new
    commit12

---
+++ build/precache-manifest.066481f34d566209aa2751961be70894.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d1cd672a790151e491da89c52c930b",
+    "url": "/static/media/logo.d6d1cd67.svg"
+  },
+  {
+    "revision": "61a4a352703bbd05cf2a",
+    "url": "/static/css/main.a08c9ee3.chunk.css"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "44f5b0061a8c0b084c60",
+    "url": "/static/js/1.44f5b006.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a334fc1bd5cda506bb5d9c87226dbbbe",
+    "url": "/static/media/ganesh.a334fc1b.jpg"
+  },
+  {
+    "revision": "61a4a352703bbd05cf2a",
+    "url": "/static/js/main.61a4a352.chunk.js"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "44f5b0061a8c0b084c60",
+    "url": "/static/css/1.0a679dbc.chunk.css"
+  },
+  {
+    "revision": "5da7a99322c26df433aa1b55ee9036a5",
+    "url": "/index.html"
+  }
+];
